30ef83a45df18f3202f2935e0c1850b5ea046c88
Anthony Croshaw <anthonycroshaw@anthonys-mbp.home>
2022-03-21 17:00:15 +0000

    added components

---
+++ src/components/handicapDetails.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "handicapDetails"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/handicapList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "handicapsList"
+}
+</script>
+
+<style scoped>
+
+</style>
